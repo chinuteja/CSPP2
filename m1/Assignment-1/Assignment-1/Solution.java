344b21f53b72aa0424593325b2d23f88064398fe
chinuteja <chinuteja2008@msitprogram.net>
2018-08-27 20:58:47 +0530

    testing

--- m1/Assignment-1/Assignment-1/Solution.java
+++ m1/Assignment-1/Assignment-1/Solution.java
@@ -2,8 +2,11 @@ import java.util.*;
 public class Solution {
 	public static void main(String[] args) {
 		Scanner key = new Scanner(System.in);
-		String x = key.nextLine();
+		
 		//System.out.println("enter Roll Number");
+		
+		
+		String x = key.nextLine();
 		int y = key.nextInt();
 
 		//System.out.println("enter Name");
