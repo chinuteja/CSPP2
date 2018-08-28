a9c61c38c4c7b47719bab043e2180e71d5ac3e2a
chinuteja <chinuteja2008@msitprogram.net>
2018-08-28 14:30:42 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

---
+++ m2/Assignment-2/Solution.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Solution {
+	/*
+	Do not modify this main function.
+	*/
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int a = scan.nextInt();
+		int b = scan.nextInt();
+		int c = scan.nextInt();
+		rootsOfQuadraticEquation(a, b, c);
+	}
+	public static void rootsOfQuadraticEquation(int a, int b, int c)
+	{
+		double x,y,denominator,r;
+		r = ((b*b) - (4*a*c));
+		denominator = 2*a;
+		x = (-b + Math.sqrt(r))/denominator;
+		y = (-b - Math.sqrt(r))/denominator;
+		System.out.println("the roots are:" +x+" "+y);
+				
+        
+	}
+}
