65fb9226c562b44c044d3ddc531cde5ae4745441
chinuteja <chinuteja2008@msitprogram.net>
2018-08-29 22:23:30 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- m3/Assignment-3/Assignment-3/Solution.java
+++ m3/Assignment-3/Assignment-3/Solution.java
@@ -38,12 +38,11 @@ public class Solution {
      * @return     { description_of_the_return_value }
      */
     public static int gcd(int n1, int n2){
-        if (n2 == 0){
-            return n1;
+        if (n2 != 0){
+            return gcd(n2, n1%n2);
         }
         else{
-            return gcd(n1, n2%n1);
-            
+            return n1;
         
         }
     }
