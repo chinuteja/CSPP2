77e2d11094e47865b5bab36af455228bce193d14
chinuteja <chinuteja2008@msitprogram.net>
2018-08-29 20:06:46 +0530

    convert.java

---
+++ m3/convert.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class convert {
+	public static void main(String[] args) {
+		int i = 2;
+		while(i<=10){
+			System.out.println(i);
+			i++;
+		}
+		System.out.println("GoodBye!");
+	}
+}
