eb8132fc36575baa5b52e04939e731fc13e4d7e9
chinuteja <chinuteja2008@msitprogram.net>
2018-08-29 22:31:35 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- m3/Assignment-3/Assignment-3/Solution.java
+++ m3/Assignment-3/Assignment-3/Solution.java
@@ -1,4 +1,6 @@
-
+/**
+ * @author Teja
+ */
 import java.util.Scanner;
 /**.
  * to find gcd of given two numbers
@@ -21,9 +23,9 @@ public final class Solution {
      * @param      args  The arguments
      */
 
-    public static void main(String[] args) {
+    public static void main(final String[]  args) {
 
-        Scanner s=new Scanner(System.in);      
+        Scanner s = new Scanner(System.in);
         int n1 = s.nextInt();
         int n2 = s.nextInt();
         //gcd(n1,n2);
@@ -40,11 +42,11 @@ public final class Solution {
      *
      * @return     { description_of_the_return_value }
      */
-    public static int gcd(int n1, int n2){
-        if (n2 != 0){
-            return gcd(n2, n1%n2);
+    public static int gcd(final int n1, final int n2) {
+        if (n2 != 0) {
+            return gcd(n2, n1 % n2);
         }
-        else{
+        else {
             return n1;
         
         }
