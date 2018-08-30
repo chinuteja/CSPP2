febf620261ed029d987ad2ef07ef5200a250819c
chinuteja <chinuteja2008@msitprogram.net>
2018-08-30 13:58:21 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- m4/Assignment-1/Assignment-1/Solution.java
+++ m4/Assignment-1/Assignment-1/Solution.java
@@ -1,36 +1,36 @@
 /**
  * @author teja
  */
-import java.util.*;
-public class Solution
-{/*
-	Fill this main function to print maximum of given array
-	*/
-	private Solution() {
-		/**
-		 * this is private constructor
-		 */
+import java.util.Scanner;
+public final class Solution {
+/*
+    Fill this main function to print maximum of given array
+    */
+    private Solution() {
+        /**
+         * this is private constructor
+         */
 
-	}
-	/**
-	 * program to find maximum element in a array
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(final String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int j;
-		int n = sc.nextInt();
-		int arr[] = new int[n];
-		for (int i=0; i<arr.length; i++) {
-			arr[i] = sc.nextInt();
-		}
-		for (j=0; j<arr.length-1; j++) {
-			if(arr[j] > arr[j+1]) {
-				arr[j+1] = arr[j];
-			}
-		
-		}
-		System.out.println(arr[j]);
-	}
+    }
+    /**.
+     * program to find maximum element in a array
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int j;
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        for (j = 0; j < arr.length - 1; j++) {
+            if(arr[j] > arr[j+1]) {
+                arr[j+1] = arr[j];
+            }
+        
+        }
+        System.out.println(arr[j]);
+    }
 }
