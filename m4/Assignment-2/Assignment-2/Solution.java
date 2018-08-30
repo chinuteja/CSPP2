029edd48d4650ccbe8b3a085e00af64424e18734
chinuteja <chinuteja2008@msitprogram.net>
2018-08-30 19:34:21 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- m4/Assignment-2/Assignment-2/Solution.java
+++ m4/Assignment-2/Assignment-2/Solution.java
@@ -3,15 +3,15 @@
  *@author : Teja
  */
 import java.util.Scanner;
-/**
+/**.
  * Class for solution.
  */
 public class Solution {
-    /**
+    /**.
      * { var_description }.
      */
-    public static final Scanner s = new Scanner(System.in);
-    /**
+    public static final Scanner scan = new Scanner(System.in);
+    /**.
      * Reads a matrix.
      *
      * @param      s1    The s 1
@@ -23,22 +23,26 @@ public class Solution {
         int[][] arr1 = new int[s1][s2];
         for (int i = 0; i < s1; i++) {
             for (int j = 0; j < s2; j++) {
-                arr1[i][j] = s.nextInt();
+                arr1[i][j] = scan.nextInt();
             }
         }
         return arr1;
     }
+    /**.
+     * function to add two matrices
+     *
+     * @param      args  The arguments
+     */
     public static void main(final String[] args) {
-        int s1 = s.nextInt();
-        int s2 = s.nextInt();
+        int s1 = scan.nextInt();
+        int s2 = scan.nextInt();
         int[][] arr1 = readMatrix(s1, s2);
-        int s3 = s.nextInt();
-        int s4 = s.nextInt();
+        int s3 = scan.nextInt();
+        int s4 = scan.nextInt();
         int[][] arr2 = readMatrix(s3, s4);
         int[][] arr = new int[s1][s2];
         if (s1 != s3 && s2 != s4) {
-            System.out.println("not possible");
-        }
+            System.out.println("not possible"); }
         else {
             for (int i = 0; i < s1; i++) {
                 for (int j = 0; j < s2; j++) {
