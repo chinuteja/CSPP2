95a423360dd18d9dac3a20c9a2c506b347fefedd
chinuteja <chinuteja2008@msitprogram.net>
2018-09-06 14:22:47 +0530

    "check_style_score -> 6 of 6 passed. style score: 0/1 "

--- home work/Assignment-3/Solution.java
+++ home work/Assignment-3/Solution.java
@@ -330,7 +330,8 @@ public class Solution {
                         listStudent.remove(Integer.parseInt(tokens[1]));
                         break;
                         case "indexOf":
-                        System.out.println(listStudent.indexOf(new Student(tokens[1])));
+                        System.out.println(
+                            listStudent.indexOf(new Student(tokens[1])));
                         break;
                         case "get":
                         System.out.println(listStudent.get(Integer.parseInt(tokens[1])));
