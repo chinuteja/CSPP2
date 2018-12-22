42af621eff6b8b87e03fae9aa2abc381dfb815fb
chinuteja <chinuteja2008@msitprogram.net>
2018-12-22 16:42:03 +0530

    "cspp2_m10_a1_01: 2/8 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- CSPP2 Remedial Exam/CSPP2 Remedial Exam/Hotel.java
+++ CSPP2 Remedial Exam/CSPP2 Remedial Exam/Hotel.java
@@ -1,3 +1,4 @@
+import java.util.*;
 class Hotel {
 	private Reservation[] rooms;
 	private int rnum = 1;
@@ -14,11 +15,11 @@ class Hotel {
 		for (int i = 0; i < rooms.length; i++) {
 			if (rooms[i] == null) {
 				reserveObj = new Reservation(person);
-				reserveObj.setRoom(i+1);
+				reserveObj.setRoom(i + 1);
 				rooms[i] = reserveObj;
 				// rnum++;
 				size++;
-				return i+1;
+				return i + 1;
 			}
 		}
 		return -1;
@@ -27,28 +28,44 @@ class Hotel {
 		if (size == rooms.length) {
 			System.out.println("All Rooms are reserved");
 		}
-
-		if (rnum == roomNum) {
-			System.out.println("Room is already reserved");
-		}
 		if (rooms[roomNum - 1] == null) {
 			reserveObj = new Reservation(person, roomNum);
 			rooms[roomNum  - 1] = reserveObj;
 			rnum++;
 			size++;
 			return true;
+		} else {
+			System.out.println("Already reserved");
 		}
 
 		return false;
 	}
 	public void printReservations() {
-		
+
 		for (int i = 0; i < size; i++) {
 			if (rooms[i] != null) {
 				System.out.println(rooms[i].getName() + " " + rooms[i].getRoom());
-			} else {
-				System.out.println((i + 1) + " is not reserved");
 			}
 		}
+	}
+	public void cancelReservation(String person) {
+		for (int i = 0; i < size; i++ ) {
+			if (rooms[i] != null) {
+				if (rooms[i].getName().equals(person)) {
+					rooms[i] = null;
+				}
+			}
+
+		}
+	}
+	public void buildRoom(int num) {
+		if (num < 0) {
+			return;
+		}
+		rooms = Arrays.copyOf(rooms, size + num);
+		for (int i = 0; i < num; i++) {
+			rooms[size++] = null;
+		}
+
 	}
 }
